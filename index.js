5404df2aa6610b72a3f4bf34852978522db21807
shanemarvinmay <shanemarvinmay@gmail.com>
2019-12-17 11:46:09 -0600

    node serves ui

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const fs = require('fs');
 
-app.get('/', (req,res) => {res.send('root reached');});
+app.get('/', (req,res) => {
+    // res.send('root reached');
+    const pathToFile = __dirname + '/index.html';
+    res.sendFile(pathToFile);
+});
 
 app.listen(port, ()=> {console.log(`local journal running on ${port}`);});
