f3d2f0216d878bcf39197469620d47846549e20d
shanemarvinmay <shanemarvinmay@gmail.com>
2019-12-17 13:09:44 -0600

    receiving basic text from entry post

--- index.js
+++ index.js
@@ -4,9 +4,25 @@ const port = 8080;
 const fs = require('fs');
 
 app.get('/', (req,res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
     // res.send('root reached');
     const pathToFile = __dirname + '/index.html';
     res.sendFile(pathToFile);
+});
+
+app.post('/saveEntries', (req,res) => { //saveEntries
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+    console.log("POST /saveEntries hit"); 
+    console.log(req.query);
+    res.send(req.query);
 });
 
 app.listen(port, ()=> {console.log(`local journal running on ${port}`);});
